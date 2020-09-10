36ce3c11959e51a97d6cfaea089eddb789f52529
Lodis Perkins <lodisperkins44@gmail.com>
2020-09-10 14:31:50 -0500

    Initial commit

---
+++ Assets/Scripts/CannonBallBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallBehaviour : MonoBehaviour
+{
+    public float forceOnFire = 300;
+
+    private bool _fire = false;
+    private bool _canFire = true;
+
+    private Rigidbody rigidbody = null;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            rigidbody.isKinematic = false;
+            rigidbody.AddForce(transform.forward * forceOnFire,ForceMode.Impulse);
+            _canFire = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PhysicsObjectBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PhysicsObjectBehaviour : MonoBehaviour
+{
+    public Material awakeMaterial = null;
+    public Material sleepingMaterial = null;
+
+    private Rigidbody _rigidbody = null;
+
+    private bool _wasSleeping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (_rigidbody.IsSleeping() && !_wasSleeping && sleepingMaterial != null)
+        {
+            _wasSleeping = true;
+            GetComponent<MeshRenderer>().material = sleepingMaterial;
+        }
+        if (!_rigidbody.IsSleeping() && _wasSleeping && awakeMaterial != null)
+        {
+            _wasSleeping = false;
+            GetComponent<MeshRenderer>().material = awakeMaterial;
+        }
+    }
+}
