5d50ec9b3d358b1ef938e99111e952d8f687fd65
Lodis Perkins <lodisperkins44@gmail.com>
2020-09-15 17:17:38 -0500

    Imported Unity Chan, added ragdoll physics

---
+++ Assets/Scripts/RagdollBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RagdollBehaviour : MonoBehaviour
+{
+    private Animator _animator;
+    [SerializeField]
+    private List<Rigidbody> rigidbodies = new List<Rigidbody>();
+    private CharacterController _controller;
+    public bool ragdollOn
+    {
+        get
+        {
+            return !_animator.enabled;
+        }
+        set
+        {
+            _animator.enabled = !value;
+            _controller.enabled = !value;
+            foreach(Rigidbody r in rigidbodies)
+            {
+                r.isKinematic = !value;
+            }
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        foreach(Rigidbody r in rigidbodies)
+        {
+            r.isKinematic = true;
+        }
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RagdollTriggerBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollTriggerBehaviour : MonoBehaviour
+{
+
+    public float explosiveForce;
+    public float explosionRadius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RagdollBehaviour ragdoll = other.GetComponentInParent<RagdollBehaviour>();
+        if(ragdoll)
+        {
+            ragdoll.ragdollOn = true;
+        }
+        Collider[] collisions = Physics.OverlapSphere(transform.position, 20);
+        foreach(Collider collider in collisions)
+        {
+            collider.GetComponent<Rigidbody>().AddExplosionForce(explosiveForce, transform.position, explosionRadius);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
